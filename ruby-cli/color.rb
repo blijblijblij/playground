58eb5c2e951a9efd7b0164954324649b3d1083e3
Rogier Wessel <rhwessel@xs4all.nl>
2017-03-17 09:26:45 +0100

    Colors on the cli, its no lolcat, but pretty usefull still

---
+++ ruby-cli/color.rb
@@ -0,0 +1,10 @@
+require 'rainbow'
+require 'rainbow/ext/string'
+
+puts Rainbow('this is red').red + ' and ' +
+     Rainbow('this on yellow bg').bg(:yellow) + ' and ' +
+     Rainbow('even bright underlined!').underline.bright
+
+puts 'this is red'.color(:red) + ' and ' +
+     'this on yellow bg'.background(:yellow) + ' and ' +
+     'even bright underlined!'.underline.bright
